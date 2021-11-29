9a0a62407a7b843ad9d9114c5ff083cf9d38d54a
LeahDaniel <leah.daniel1997@gmail.com>
2021-11-29 09:45:42 -0600

    chapter 4 complete

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,10 +1,16 @@
 import React from "react"
 import { LocationList } from "./location/LocationList.js"
 import "./Kennel.css"
+import { AnimalProvider } from "./animal/AnimalProvider.js"
+import { AnimalList } from "./animal/AnimalList.js"
 
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
+        <h1>Nashville Kennels</h1>
+        <AnimalProvider>
+            <AnimalList />
+        </AnimalProvider>
+
         <LocationList />
     </>
 )
